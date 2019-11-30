56883cb65a9978ce496831c594760c92d5f65e4b
Cindy Kei <myk245@nyu.edu>
2019-11-30 06:51:14 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
+
+def reverse_each_word(string)
+  new_array = string.split(" ")
+  reversed_array = new_array.collect {|x| x.reverse!}
+  return reversed_array.join (" ")
+end
