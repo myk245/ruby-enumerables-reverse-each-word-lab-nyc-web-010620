e234c600a4b1fa500e795d4ab1211424013adaf4
Cindy Kei <myk245@nyu.edu>
2019-11-30 06:44:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string)
-  array = string.split("")
-  reversed_array = array.each {|x| x.reverse!}
-  return reversed_array.join("")
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
